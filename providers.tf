8ccbeaf5d97c66ad16bbd06a4f35c107692b5d9b
Jilver <jilver.practicando@gmail.com>
2023-07-19 17:05:53 -0500

    delete key and secret

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZS4KS7CMUVU2VTHW"
-  secret_key = "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"
   default_tags {
     tags = var.tags_Virginia
   }
